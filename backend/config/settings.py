5bb0d018c04cb0fafe722617d5e4edbaeb67b80f
Jake Alvarez <yakisoba@Jakes-iMac.local>
2021-10-08 11:20:52 -0500

    changed CORS settings in settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,9 +29,10 @@ SECRET_KEY = 'django-insecure-ov#r^^&xv&^0vmc(zj&h_t^$*52@8jicn=%*z*@s-=li!s_p@=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'artshop-backend-production.herokuapp.com']
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://artshop-frontend-production.herokuapp.com']
+#ALLOWED_HOSTS = ['127.0.0.1', 'artshop-backend-production.herokuapp.com']
+ALLOWED_HOSTS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://artshop-frontend-production.herokuapp.com']
 
 # Application definition
 
