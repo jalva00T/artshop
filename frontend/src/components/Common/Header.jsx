b159d0809afd66f2fb80efa9ab9840f297557c50
Jake Alvarez <yakisoba@Jakes-iMac.local>
2021-12-01 16:29:11 -0600

    edited header

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import ImgBag from "../../assets/img/bag.png"
-
+import React from "react";
+import { useHistory } from "react-router";
+import ImgBag from "../../assets/img/bag.png";
 export default function Header() {
-    return (
-        <>
-            <header>
-                <div class="header-container">
-                    <h2 class="art-and-soul"><a href="/">Art and Soul</a></h2>
-                    <div class="right-side-nav">
-                        <a class="sign-in title-icon navbar-brand" href="/signin">Sign In</a>
-                        <a class="sign-in title-icon navbar-brand" href="/cart">
-                            <img class="bag" alt="" src={ImgBag} />
-                        </a>
-                    </div>
-                </div>
-            </header>
-        </>
-    )
+  let user = JSON.parse(localStorage.getItem("LOGIN_USER_KEY"));
+  const history = useHistory();
+  function logOut() {
+    localStorage.clear();
+    history.push("/Signin");
+  }
+  return (
+    <>
+      <header>
+        <div class="header-container">
+          <h2 class="art-and-soul">
+            <a href="/">Art and Soul</a>
+          </h2>
+          {localStorage.getItem("LOGIN_USER_KEY") ? null : (
+            <div class="right-side-nav">
+              <a class="sign-in title-icon navbar-brand" href="/signin">
+                Sign In
+              </a>
+              <a class="sign-in title-icon navbar-brand" href="/cart">
+                <img class="bag" alt="" src={ImgBag} />
+              </a>
+            </div>
+          )}
+          <div>
+            {localStorage.getItem("LOGIN_USER_KEY") ? (
+              <button onClick={logOut}>LogOut </button>
+            ) : null}
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
