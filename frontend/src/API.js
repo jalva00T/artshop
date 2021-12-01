f1f5efcafa895c43d51078203f186b859dfff7c0
Jake Alvarez <yakisoba@Jakes-iMac.local>
2021-12-01 12:21:02 -0600

    deployment

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -2,14 +2,14 @@ import axios from "axios";
 const LOGIN_USER_KEY = "LOGIN_USER_KEY";
 
 var baseURL;
-if (
-    process.env.REACT_APP_ENVIRONMENT &&
-    process.env.REACT_APP_ENVIRONMENT === "PRODUCTION"
-) {
-    baseURL = process.env.REACT_APP_API_BASE_URL;
-} else {
-    baseURL = "http://127.0.0.1:8000";
-}
+// if (
+//     process.env.REACT_APP_ENVIRONMENT &&
+//     process.env.REACT_APP_ENVIRONMENT === "PRODUCTION"
+// ) {
+//     baseURL = process.env.REACT_APP_API_BASE_URL;
+// } else {
+    baseURL = "https://artshop-backend.herokuapp.com/";
+// }
 
 const api = axios.create({
     baseURL: baseURL,
